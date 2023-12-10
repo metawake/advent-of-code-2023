119649bac44eba9949f680fafeac4e91b0881070
Alex Alexapolsky <metawake@gmail.com>
2023-12-10 21:22:31 +0100

    1 day, 1 problem

--- 1.py
+++ 1.py
@@ -1,1 +1,23 @@
-print("Hello Advent of Code!")
+def find_first_last_digit(row):
+    result = None
+
+    digits = [n for n in row if n.isdigit()]
+    result = '0'
+    if len(digits) >= 2:
+        result = digits[0] + digits[-1]
+    elif len(digits) == 1:
+        result = digits[0] + digits[0]
+
+    result = int(result)
+
+    return result
+
+with open('data/1.data','r') as f:
+    data = f.readlines()
+    nums = []
+
+    data = [d.rstrip('\n') for d in data]
+    for row in data:
+        n = find_first_last_digit(row)
+        nums.append(n)
+    print(sum(nums))
